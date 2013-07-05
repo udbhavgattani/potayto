e90cab0c4d428efc65358b919aa803f91f78d266
Scott Danzig <sdanzig@gmail.com>
2013-07-05 17:01:30 -0400

    Added one more line.

--- src/main/scala/scottdanzig/potayto/Potayto.scala
+++ src/main/scala/scottdanzig/potayto/Potayto.scala
@@ -4,7 +4,8 @@ object Potayto {
 
   def main(args: Array[String]): Unit = {
     println("Potayto potahto,")
-    println("Tomayto tomahto!")
+    println("Tomayto tomahto.")
+    println("Let's call the whole thing off.")
   }
 
 }
